b72eeff9eb2f696e14262aa8a3e5d3a9e0f95f23
275c0dfe <mgpnoahp@gmail.com>
2022-07-30 02:36:12 -0700

    Fixed setkey vulnerability

--- scripts/setkey.py
+++ scripts/setkey.py
@@ -2,5 +2,5 @@ from scripts.requestRefrences import *
 url = urlInfo.URI
 qs = urlInfo.qs
 content = ""
-if urlInfo.qs_found:
-    Data["Access_Key"] = qs["value"]
+#if urlInfo.qs_found:
+    #Data["Access_Key"] = qs["value"]
